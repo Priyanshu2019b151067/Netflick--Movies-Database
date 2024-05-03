65602f142e56f82552bfcfb003578279e8005949
Priyanshu <priyanshukingh@gmail.com>
2024-05-04 02:42:59 +0530

    redux toolit setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { useState,useEffect } from 'react'
 import { fetchdatafromapi } from './utils/api.js'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration,getGenres } from './store/homeSlice.js';
 
 
 function App() {
-  
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.home.url);
+
+  console.log(url);
   useEffect(()=>{
     // because we want the rendering to be done only at the initail render.
     apiTesting();
   },[]);
-  const apiTesting = () =>{
-      fetchdatafromapi('/movie/popular').then((data)=>{
-        console.log(data);
-      }).catch((err)=>{
-        console.log(err);
-      });
+  const apiTesting = async () =>{
+    try{
+      const data  =await fetchdatafromapi('/movie/popular');
+      console.log(data);
+      dispatch(getApiConfiguration(data));
+    }catch(err){
+      console.log(err);
+    }
   }
 
   return (
     <>
-      
+     {url?.page}
     </>
   )
 }
